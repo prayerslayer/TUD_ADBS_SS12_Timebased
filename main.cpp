5ad302902ad0a8ac091c305c7383a78d3976a40c
Kirsche3103 <marcus310389@aol.com>
2012-05-02 14:51:57 +0200

    main commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  TUD_ADBS_SS12_Timebased
+//
+//  Created by Marcus Kirsch on 02.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
